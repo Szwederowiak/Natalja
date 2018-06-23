8f14e02e05ed5f497bc26ce10d2997123d1be0b1
Szwederowiak <35521057+Szwederowiak@users.noreply.github.com>
2018-06-23 16:32:25 +0200

    asd

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var word = prompt('Podaj słowo:', '').toUpperCase();
+//var word = prompt('Podaj słowo:', '').toUpperCase();
+var word = "Kurwa kurwie łba nie urwie";
 
 var word1 = "";
 var misses = 0;
